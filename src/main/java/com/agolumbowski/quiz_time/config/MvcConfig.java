e925ad6a28808267c04ca16d6960ca2596936540
ArtemGolumbowski <a.golumbowski@gmail.com>
2021-12-06 21:13:03 +0200

    second commit

--- src/main/java/com/agolumbowski/quiz_time/config/MvcConfig.java
+++ src/main/java/com/agolumbowski/quiz_time/config/MvcConfig.java
@@ -1,16 +1,50 @@
-
 package com.agolumbowski.quiz_time.config;
 
+import java.util.Locale;
+import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-        @Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
+@Bean(name = "messageSource")
+	public MessageSource getMessageResource()  {
+		ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+		
+		// Read i18n/messages_xxx.properties file.
+		// For example: i18n/messages_en.properties
+		messageResource.setBasename("classpath:messages/lang");
+		messageResource.setDefaultEncoding("UTF-8");
+		return messageResource;
+	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
---
+++ src/main/java/com/agolumbowski/quiz_time/сontroller/UserController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agolumbowski.quiz_time.сontroller;
+
+import com.agolumbowski.quiz_time.service.TestService;
+import com.agolumbowski.quiz_time.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author agolu
+ */
+@Controller
+public class UserController {
+     @Autowired
+    private UserService userService;
+   @GetMapping("/users")
+    public String getTestsPage(Model model,@RequestParam(defaultValue="0")int page) {
+        Page userPage = userService.getAllUsers(page);
+        model.addAttribute("users", userPage);
+ 
+        return "users";
+    }
+    
+}
